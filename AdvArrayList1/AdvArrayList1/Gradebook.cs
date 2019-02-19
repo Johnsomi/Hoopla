577c6f0d693d2b7d1e2f213a6e13359f7d1e2cb7
Johnsomi <mj4100@students.inghamisd.org>
2019-02-19 12:08:46 -0500

    hgfde

---
+++ AdvArrayList1/AdvArrayList1/Gradebook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvArrayList1
+{
+    public class Gradebook
+    {
+        //properties
+        //list/array of sections
+        int currentSection;
+
+        //methods
+        //changeSection(string sectionName); sets the currently active section
+        //boolean addSection(String sectionName)//creates a new section if there are not already 6 sections and sectionName isnt already used 
+    }
+}
