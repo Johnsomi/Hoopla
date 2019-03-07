2f95980869a6cb58dfe2e54b5f7a583c05c914e3
Johnsomi <mj4100@students.inghamisd.org>
2019-03-07 13:54:18 -0500

    bter

--- AdvArraysList2/AdvArraysList2/Program.cs
+++ AdvArraysList2/AdvArraysList2/Program.cs
@@ -9,7 +9,82 @@ namespace AdvArraysList2
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            List<Users> users = new List<Users>();
+
+            //Console.WriteLine("Would you like to (create account) or (login)?");
+            while (true)
+            {
+                Console.WriteLine("Would you like to (create account) or (login)?");
+                string LogOrCA = Console.ReadLine();
+
+                if(LogOrCA == "create account")
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Enter Username: ");
+                        string UsernameC = Console.ReadLine();
+                        //check if username exists
+                        bool existsC = false;
+                        int K = 0;
+                        while (K < users.Count)
+                        {
+                            Users U;
+                            U = users[0];
+                            if (U.username.Equals(UsernameC))
+                            {
+                                existsC = true;
+                                Console.WriteLine("User already exists");
+                                break;
+                            }
+                            K = K + 1;
+                        }
+                        if (existsC == false)
+                        {
+                            Console.WriteLine("Enter Password: ");
+                            string PasswordC = Console.ReadLine();
+                            users.Add(new Users(UsernameC, PasswordC));
+                            break;
+                        }                       
+                    }
+                }
+
+                if(LogOrCA == "login")
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Enter Username: ");
+                        string UsernameL = Console.ReadLine();
+                        Console.WriteLine("Enter Password: ");
+                        string PasswordL = Console.ReadLine();
+
+                        bool existsL = false;
+                        int J = 0;
+                        while (J < users.Count)
+                        {
+                            Users U;
+                            U = users[0];
+                            if (U.username.Equals(UsernameL) && U.password.Equals(PasswordL))
+                            {
+                                existsL = true;
+                                Console.WriteLine("Welcome, " + UsernameL);
+                                Console.ReadKey();
+                                Environment.Exit(0);
+                            }
+                        }
+                        if(existsL == false)
+                        {
+                            Console.WriteLine("Sorry, account not in records.");
+                            break;
+                        }
+                    }
+                }
+
+                else
+                {
+                    continue;
+                }
+            }
         }
     }
 }
---
+++ AdvArraysList2/AdvArraysList2/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvArraysList2
+{
+    class Users
+    {
+        public string username;
+        public string password;
+
+        public Users(string Username, string Password)
+        {
+            username = Username;
+            password = Password;
+        }
+    }
+}
